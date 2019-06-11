d27b00139fdc35ea59c46066c5b35cb0ed82e6fc
jingbest <43697769@qq.com>
2019-06-11 16:04:16 +0800

    console.log

--- frontend/public/components/utils/gettext.js
+++ frontend/public/components/utils/gettext.js
@@ -3,12 +3,17 @@ import cookie from 'js-cookie';
 import locale from './locale';
 
 export const gettext = (str, value) => {
-  if (!window.locale) {
-    window.locale = cookie.get('openshift_language') || 'zh-cn';
+  try {
+    if (!window.locale) {
+      window.locale = cookie.get('openshift_language') || 'zh-cn';
+    }
+    const locales = locale[window.locale];
+    if (!value) {
+      return locales && locales[str] ? locales[str] : str;
+    }
+    return locales && locales[str] ? locales[str].replace('{0}', value) : str.replace('%s', value);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error);
   }
-  const locales = locale[window.locale];
-  if (!value) {
-    return locales && locales[str] ? locales[str] : str;
-  }
-  return locales && locales[str] ? locales[str].replace('{0}', value) : str.replace('%s', value);
 };
